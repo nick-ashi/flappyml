a99f6af78b0703b9051db85f97ea36742d7d080c
Nick Ashizawa <nickashi2002@gmail.com>
2024-06-30 22:25:46 -0700

    partially done with main func

--- flappyml.py
+++ flappyml.py
@@ -4,7 +4,7 @@ import time
 import random
 import os
 
-WIN_WIDTH = 600
+WIN_WIDTH = 500
 WIN_HEIGHT = 800
 
 #loading images
@@ -29,4 +29,81 @@ class Bird:
         self.height = self.y
         self.img_count = 0
         self.img = self.IMGS[0]
+    def jump(self):
+        self.vel = -10.5 #moving upwards toward y = 0
+        self.tick_count = 0
+        self.height = self.y
+
+    def move(self):
+        self.tick_count += 1 #a frame went by, we moved
+        
+        d = self.vel * self.tick_count + 1.5 * self.tick_count**2
+
+        if d >= 16:
+            d = 16
+        if d < 0:
+            d -= 2 #if we're moving upwards, move up a bit more to make the jump a bit nicer
+        
+        self.y = self.y + d #move slowly up or slowly down
+
+        if d < 0 or self.y < self.height + 50:
+            if self.tilt < self.MAX_ROTATION:
+                self.tilt = self.MAX_ROTATION
+        else:
+            if self.tilt > -90:
+                self.tilt -= self.ROT_VEL
+        
+    def draw(self, win):
+        self.img_count += 1
+
+        if self.img_count < self.ANIMATION_TIME:
+            self.img = self.IMGS[0]
+        elif self.img_count < self.ANIMATION_TIME*2:
+            self.img = self.IMGS[1]
+        elif self.img_count < self.ANIMATION_TIME*3:
+            self.img = self.IMGS[2]
+        elif self.img_count < self.ANIMATION_TIME*4:
+            self.img = self.IMGS[1]
+        elif self.img_count == self.ANIMATION_TIME*4 + 1:
+            self.img = self.IMGS[0]
+            self.img_count = 0
+
+        if self.tilt <= -80:
+            self.img = self.IMGS[1] #should look like it's nosediving
+            self.img_count = self.ANIMATION_TIME*2
+
+        rotated_image = pygame.transform.rotate(self.img, self.tilt)
+        new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
+
+        win.blit(rotated_image, new_rect.topleft)
+
+    def get_mask(self):
+        return pygame.mask.from_surface(self.img)
+    
+
+def draw_window(win, bird):
+    win.blit(BG_IMG, (0, 0))
+    bird.draw(win)
+    pygame.display.update()
+
+def main():
+    bird = Bird(200, 200)
+    win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
+    run = True
+
+    while run:
+        clock.tick(30)
+        # keep track of something the user does like click the mous
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:
+                run = False
+        #bird.move()
+        draw_window(win, bird)
+    
+    pygame.quit()
+    quit()
+        
+main()
+
 
