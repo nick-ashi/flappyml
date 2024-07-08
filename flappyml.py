cd8581993943aa894daff6e1ca8dc0cdc68cc145
Nick Ashizawa <nickashi2002@gmail.com>
2024-07-08 11:25:03 -0700

    update for using NEAT

--- flappyml.py
+++ flappyml.py
@@ -1,3 +1,4 @@
+import neat.population
 import pygame
 import neat
 import time
@@ -167,9 +168,19 @@ def draw_window(win, bird, pipes, base, score):
     bird.draw(win)
     pygame.display.update()
 
-def main():
-    score = 0
-    bird = Bird(230, 350)
+def main(genomes, config):
+    nets = [] #networks
+    ge = [] #genomes
+    birds = []
+
+    for g in genomes:
+        net = neat.nn.FeedForwardNetwork(g, config)
+        nets.append(net)
+        birds.append(Bird(230, 350))
+        g.fitness = 0
+        ge.append(g)
+
+
     base = Base(730)
     pipes = [Pipe(700)]
     win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -182,31 +193,46 @@ def main():
         for event in pygame.event.get(): 
             if event.type == pygame.QUIT:
                 run = False
+
+        pipe_ind = 0
+        if len(birds) > 0:
+            if len(pipes) > 1 and birds[0].x > pipes[0].x + pipes[0].PIPE_TOP.get_width():
+                pipe_ind = 1
+
         #bird.move()
         rem = [] #removed pipes
         add_pipe = False
         for pipe in pipes:
-            pipe.move()
-            if pipe.collide(bird):
-                pass
+            for x, bird in enumerate(birds):
+                if pipe.collide(bird):
+                    ge[x].fitness -= 1
+                    birds.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+                if not pipe.passed and pipe.x < bird.x:
+                    pipe.passed = True
+                    add_pipe = True
+
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                 rem.append(pipe)
-
-            if not pipe.passed and pipe.x < bird.x:
-                pipe.passed = True
-                add_pipe = True
 
             pipe.move()
         
         if add_pipe:
             score += 1
+            for g in ge:
+                g.fitness += 5 # if it's still alive give it some fit score
             pipes.append(Pipe(600))
 
         for r in rem:
             pipes.remove(r)
-
-        if bird.y + bird.img.get_height() > 730:
-            pass
+        
+        for x, bird in enumerate(birds):
+            if bird.y + bird.img.get_height() > 730:
+                birds.pop(x)
+                nets.pop(x)
+                ge.pop(x)
 
         base.move()
         draw_window(win, bird, pipes, base, score)
@@ -219,7 +245,15 @@ main()
 def run(config_path):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation, config_path)
 
+    p = neat.population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, 50)
+
 if __name__ == "__main__":
-    local_dir = os.path.dirname(__file__) #give us the path to current dir to load in the config file
+    local_dir = os.path.dirname(__file__) # give us the path to current dir to load in the config file
     config_path = os.path.join(local_dir, "config-feedforward.txt")
     run(config_path)
